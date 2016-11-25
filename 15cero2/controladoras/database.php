dcfe5770e82133d9e195bf53e685682890b32c7e
gsgianks <gsgiank@gmail.com>
2016-11-24 18:17:49 -0600

    root

--- 15cero2/controladoras/database.php
+++ 15cero2/controladoras/database.php
@@ -3,7 +3,7 @@
 function getConnection(){
     $servername = "localhost";
     $username = "root";
-    $password = "root";
+    $password = "";
     $dbname = "db15cero2";
 
     // Create connection
