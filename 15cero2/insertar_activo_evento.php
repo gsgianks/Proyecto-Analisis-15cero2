85ffcd5c6b7d2e8b3f4f8470d478c51bc2d622f6
gsgianks <gsgiank@gmail.com>
2016-11-29 23:01:27 -0600

    activo_evento

--- 15cero2/insertar_activo_evento.php
+++ 15cero2/insertar_activo_evento.php
@@ -4,6 +4,7 @@
 	<title>Insertar Activo a Evento</title>
 	<link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/newStyle.css">
+    <link rel="stylesheet" href="css/activos.css">
     <link rel="stylesheet" href="js/css/alertify.min.css">
     <link rel="stylesheet" href="js/css/themes/default.min.css">
 
@@ -16,42 +17,51 @@
 <body>
 	<?php include "_nav.php" ?>
 	<div class="main_content">
+		<aside class="aside-left">
+            <section class="aside-content">
+    			<section class="formulario">
+    				
+					<h1>Insertar Activos a Evento</h1>
+					<form action="controladoras/controladora_evento_activo.php" method="post">
+
+						<input type="hidden" name="evento" value='<?php echo $_GET["e"] ?>'>
+				        <input type="hidden" name="consulta" value='agregarActivosEvento'>
+				        <div class="form-group"><label>Categorias:</label><br>
+				        <select class="form-control" id="categorias" name="categoria">
+				        	<option value="0">--Seleccione--</option>
+				        	<?php require_once 'controladoras/controladora_categoria.php'; $Categorias = cargarCategorias2();
+							for($i = 0; $i<count($Categorias);$i++){ ?>
+								<option value="<?php echo $Categorias[$i][0]?>"><?php echo $Categorias[$i][1]?></option>
+							<?php } ?>
+				        </select></div>
+				        <div class="form-group"><label>Activo: </label><br>
+						<select class="form-control" id="activos" name="activo">
+							<option value="0">-- No hay activos --</option>
+						</select></div>
+						<div class="form-group"><label>Cantidad:</label><br>
+						<input class="form-control" id="cantidad_activos" type="number" name="cantidad" required><div class="form-group">
+						Disponibles: <label style="color: blue;" id="cantidad_maxima"></label><br>
+						<button class="submit btn btn-primary" type="submit">Agregar</button> 
+					</form>
+					
+    			</section>
+            </section>
+		</aside>
 		<h3 id="Event">Nombre evento:<?php echo $_GET['n'] ?></h3>
-		<div class="lista-activos">
-		<form action="controladoras/controladora_evento_activo.php" method="post">
-
-			<input type="hidden" name="evento" value='<?php echo $_GET["e"] ?>'>
-	        <input type="hidden" name="consulta" value='agregarActivosEvento'>
-	        <label>Categorias:</label><br>
-	        <select id="categorias" name="categoria">
-	        	<option value="0">--Seleccione--</option>
-	        	<?php require_once 'controladoras/controladora_categoria.php'; $Categorias = cargarCategorias2();
-				for($i = 0; $i<count($Categorias);$i++){ ?>
-					<option value="<?php echo $Categorias[$i][0]?>"><?php echo $Categorias[$i][1]?></option>
-				<?php } ?>
-	        </select><br>
-	        <label>Activo: </label><br>
-			<select id="activos" name="activo">
-				<option value="0">-- No hay activos --</option>
-			</select><br>
-			<label>Cantidad:</label><br>
-			<input id="cantidad_activos" type="number" name="cantidad" required><br>
-			Disponibles: <label style="color: blue;" id="cantidad_maxima"></label><br>
-			<input class="submit" type="submit" value="Agregar">
-		</form>
-		</div>
-
 		<div class="activos-necesarios">
+			
 			<?php require_once 'controladoras/controladora_evento_activo.php'; $eventos = seleccionarActivosEventosArray($_GET["e"]);
 			if($eventos != null){ ?>
 				<h3>Eventos</h3>
-	            <table id="table" border="2">
+	            <table id="table">
+	            <thead>
 		            <tr>
 		                <td>Id</td>
 		                <td>Nombre</td>
 		                <td>Cantidad</td>
 		                <td>Acci&oacute;n</td>
 		            </tr>
+		            </thead>
 		            <?php for($i = 0;$i<count($eventos);$i++){ ?>
 		                <tr>
 		                	<td><?php echo $eventos[$i][2]; ?></td>
